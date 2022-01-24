8ae28c39eee028d800096b33edab5df9d67a4d68
Peter Compton-Burnett <compton-burnett.p@etoncollege.org.uk>
2022-01-24 19:07:30 +0000

    Initial Commit

---
+++ AbsenseRecorder/AbsenseRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenseRecorderApp.swift
+//  AbsenseRecorder
+//
+//  Created by Compton-Burnett, Peter (PGW) on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenseRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
