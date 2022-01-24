3812504adb45ed1dd7d0af8f74917a31b66f6ae5
Peter Compton-Burnett <compton-burnett.p@etoncollege.org.uk>
2022-01-24 20:43:59 +0000

    Adding Basic model for divisions and students

---
+++ AbsenseRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenseRecorder
+//
+//  Created by Compton-Burnett, Peter (PGW) on 24/01/2022.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
